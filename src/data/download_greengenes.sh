833065dbdf7d9c9ebf57f320b0a6f9b1bcaac7d7
eitan <eitan.turok@gmail.com>
2022-11-21 00:09:22 -0500

    download greengenes 13_5 instead of 12_10

--- src/data/download_greengenes.sh
+++ src/data/download_greengenes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Download the Greengenes dataset as a FASTA file
-# More info: https://greengenes.secondgenome.com/?prefix=downloads/greengenes_database/gg_12_10/
+# More info: https://greengenes.secondgenome.com/?prefix=downloads/greengenes_database/gg_13_5/
 
 # Specify the url and destination
-url='https://gg-sg-web.s3-us-west-2.amazonaws.com/downloads/greengenes_database/gg_12_10/gg_12_10.fasta.gz'
-destination='data/raw/fasta'
+url='https://gg-sg-web.s3-us-west-2.amazonaws.com/downloads/greengenes_database/gg_13_5/gg_13_5.fasta.gz'
+destination='data/raw/greengenes'
 
 # Make directory if it doesn't exist
 mkdir -p $destination
