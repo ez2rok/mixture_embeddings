4281a95dc67c655918b1411e6105af7dbf515d8a
eitan <eitan.turok@gmail.com>
2022-11-20 19:30:13 -0500

    clean up the download greengenes file

--- src/data/download_greengenes.sh
+++ src/data/download_greengenes.sh
@@ -4,7 +4,7 @@
 
 # Specify the url and destination
 url='https://gg-sg-web.s3-us-west-2.amazonaws.com/downloads/greengenes_database/gg_12_10/gg_12_10.fasta.gz'
-destination='data/raw/greengenes'
+destination='data/raw/fasta'
 
 # Make directory if it doesn't exist
 mkdir -p $destination
@@ -12,4 +12,4 @@ mkdir -p $destination
 # Download greengenes and unzip it in the `data/raw/greengenes` directory
 wget -v $url -P $destination
 gzip -dkrvc $destination/gg_12_10.fasta.gz > $destination/gg_12_10.fasta
-rm data/raw/greengenes/gg_12_10.fasta.gz
+rm $destination/gg_12_10.fasta.gz
