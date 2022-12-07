e3eca47276d7508fd03c55bcdfbbf5703647457c
eitan <eitan.turok@gmail.com>
2022-12-07 18:40:23 -0500

    normalize OTU tables in parse_moms_pi.py instead of in otu_to_mixture_embeddings.py

--- src/data/otu_to_mixture_embeddings.py
+++ src/data/otu_to_mixture_embeddings.py
@@ -25,33 +25,12 @@ otu_type_to_sample_body_site = {
 }
 
 
-def load(otu_table_path, id_to_embedding_path, model_path, sample_data_to_sample_id_path):
-    otu_tables = load_dataset(otu_table_path)
+def load(otu_tables_path, id_to_embedding_path, model_path, sample_data_to_sample_id_path):
+    otu_tables = load_dataset(otu_tables_path)
     id_to_embedding = load_dataset(id_to_embedding_path)
     _, _, _, distance_str, radius, _ = torch.load(model_path)
     sample_data_to_sample_id = load_dataset(sample_data_to_sample_id_path)
     return otu_tables, id_to_embedding, distance_str, radius, sample_data_to_sample_id
-    
-    
-def normalize_otu_table(otu_tables):
-    return {otu_type: otu_table.norm() for otu_type, otu_table in otu_tables.items()}
-
-
-def drop_missing_ids(otu_tables, id_to_embedding, verbose=False):
-    "Remove ids from the OTU matrix that are not found in the greengenes dataset."
-    
-    otu_tables_cleaned = {}
-    for otu_type, otu_table in otu_tables.items():
-        
-        ids = otu_table.ids(axis='observation')
-        valid_ids = [id_ for id_ in ids if int(id_) in id_to_embedding]
-        otu_table_valid = otu_table.filter(valid_ids, axis='observation', inplace=False)
-        otu_tables_cleaned[otu_type] = otu_table_valid
-        
-        if verbose:
-            valid_ratio = len(valid_ids) / len(ids)
-            print(f'{otu_type}: {valid_ratio:.2%} of ids are valid')
-    return otu_tables_cleaned
 
 
 def get_normalized_embeddings(otu_tables, id_to_embedding, distance_str, radius):
@@ -109,33 +88,29 @@ def save(sample_id_to_mixture_embedding, path):
     return path
 
 
-def main(moms_pi_tables_path, id_to_embedding_path, model_path, out_path, sample_data_to_sample_id_path):
+def main(otu_tables_path, id_to_embedding_path, model_path, out_path, sample_data_to_sample_id_path):
     """Compute the mixture embeddings for each sample. Return a dictionary mapping sample ids to
     embeddings.
     
     For each sample body site, compute the Frechet mean of the embeddings of
     each OTU weighted by the relative abundance of the OTU in the sample."""
     
-    results = load(moms_pi_tables_path, id_to_embedding_path, model_path, sample_data_to_sample_id_path)
+    results = load(otu_tables_path, id_to_embedding_path, model_path, sample_data_to_sample_id_path)
     otu_tables, id_to_embedding, distance_str, radius, sample_data_to_sample_id = results
-    otu_tables_normed = normalize_otu_table(otu_tables)
-    otu_tables_cleaned = drop_missing_ids(otu_tables_normed, id_to_embedding)
-    embeddings_normed = get_normalized_embeddings(otu_tables_cleaned, id_to_embedding, distance_str, radius)
+    embeddings_normed = get_normalized_embeddings(otu_tables, id_to_embedding, distance_str, radius)
     
     print('Computing mixture embeddings seperately for each of the 10 sample body sites...')
-    sample_id_to_mixture_embedding = get_mixture_embeddings(otu_tables_cleaned, embeddings_normed, sample_data_to_sample_id)
+    sample_id_to_mixture_embedding = get_mixture_embeddings(otu_tables, embeddings_normed, sample_data_to_sample_id)
     save(sample_id_to_mixture_embedding, out_path)
     return sample_id_to_mixture_embedding
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--moms_pi_tables_path', type=str, default='data/interim/moms_pi/16s_tables.pkl')
+    parser.add_argument('--otu_tables_path', type=str, default='data/interim/moms_pi/otu_tables_normed_and_cleaned.pickle')
     parser.add_argument('--id_to_embedding_path', type=str, default='data/processed/greengenes/mlpencoder_id_to_embedding.pickle')
     parser.add_argument('--model_path', type=str, default='models/MLPEncoder.pickle')
     parser.add_argument('--sample_data_to_sample_id_path', type=str, default='data/interim/moms_pi/sample_data_to_sample_id.pickle')
     parser.add_argument('--out_path', type=str, default='data/processed/greengenes/sample_id_to_mixture_embedding.pickle')
     args = parser.parse_args()
     
-    main(args.moms_pi_tables_path, args.id_to_embedding_path, args.model_path, args.out_path, args.sample_data_to_sample_id_path)
-    
-    
+    main(args.otu_tables_path, args.id_to_embedding_path, args.model_path, args.out_path, args.sample_data_to_sample_id_path)
--- src/data/parse_moms_pi.py
+++ src/data/parse_moms_pi.py
@@ -5,10 +5,13 @@ import argparse
 from util.data_handling.data_loader import load_dataset
 
 
-def load(metadata_path, manifest_path):
+def load(auxillary_data_path, otu_tables_path, metadata_path, manifest_path):
+    auxillary_data = load_dataset(auxillary_data_path)
+    id_to_str_seq = auxillary_data[0]
+    otu_tables = load_dataset(otu_tables_path)
     metadata = pd.read_csv(metadata_path, sep='\t')
     manifest = pd.read_csv(manifest_path, sep='\t')
-    return metadata, manifest
+    return otu_tables, metadata, manifest, id_to_str_seq
 
 
 def get_sample_data_to_sample_id(metadata, manifest):
@@ -22,26 +25,58 @@ def get_sample_data_to_sample_id(metadata, manifest):
     return sample_data_to_sample_id
 
 
+
+
+
+def normalize_otu_table(otu_tables):
+    return {otu_type: otu_table.norm() for otu_type, otu_table in otu_tables.items()}
+
+
+def drop_missing_ids(otu_tables, id_to_str_seq, verbose=False):
+    "Remove ids from the OTU matrix that are not found in the greengenes dataset."
+    
+    otu_tables_cleaned = {}
+    for otu_type, otu_table in otu_tables.items():
+        
+        ids = otu_table.ids(axis='observation')
+        valid_ids = [id_ for id_ in ids if int(id_) in id_to_str_seq]
+        otu_table_valid = otu_table.filter(valid_ids, axis='observation', inplace=False)
+        otu_tables_cleaned[otu_type] = otu_table_valid
+        
+        if verbose:
+            valid_ratio = len(valid_ids) / len(ids)
+            print("{}: drop {:.2%} of OTUs (because their IDs aren't in greengenes)".format(otu_type, 1 - valid_ratio))
+    return otu_tables_cleaned
+
+
 def save(sample_data_to_sample_id, path):
     with open(path, 'wb') as f:
         pickle.dump(sample_data_to_sample_id, f)
     return path
 
 
-def main(metadata_path, manifest_path, out_path):
-    """Map the sample data (subject_id, visit_number, sample_body_site) to the sample id."""
+def main(auxillary_data_path, otu_tables_path, metadata_path, manifest_path, out_dir):
+    """ Normalize the OTU tables and remove ids that are not found in the greengenes dataset.
+    Map the sample data (subject_id, visit_number, sample_body_site) to the sample id."""
     
-    metadata, manifest = load(metadata_path, manifest_path)
+    results = load(auxillary_data_path, otu_tables_path, metadata_path, manifest_path)
+    otu_tables, metadata, manifest, id_to_str_seq = results
+    
+    otu_tables_normed = normalize_otu_table(otu_tables)
+    otu_tables_normed_and_cleaned = drop_missing_ids(otu_tables_normed, id_to_str_seq, verbose=True)    
     sample_data_to_sample_id = get_sample_data_to_sample_id(metadata, manifest)
-    save(sample_data_to_sample_id, out_path)
-    return sample_data_to_sample_id
     
+    save(otu_tables_normed_and_cleaned, out_dir + 'otu_tables_normed_and_cleaned.pickle')
+    save(sample_data_to_sample_id, out_dir + 'sample_data_to_sample_id.pickle')
+    return otu_tables_normed_and_cleaned, sample_data_to_sample_id
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--auxillary_data_path', type=str, default='data/interim/greengenes/auxillary_data.pickle')
+    parser.add_argument('--otu_tables_path', type=str, default='data/interim/moms_pi/16s_tables.pkl')
     parser.add_argument('--metadata_path', type=str, default='data/interim/moms_pi/16s_metadata.tsv')
     parser.add_argument('--manifest_path', type=str, default='data/interim/moms_pi/16s_manifest.tsv')
-    parser.add_argument('--out_path', type=str, default='data/interim/moms_pi/sample_data_to_sample_id.pickle')
+    parser.add_argument('--out_dir', type=str, default='data/interim/moms_pi/')
     args = parser.parse_args()
     
-    main(args.metadata_path, args.manifest_path, args.out_path)
+    main(args.auxillary_data_path, args.otu_tables_path, args.metadata_path, args.manifest_path, args.out_dir)
