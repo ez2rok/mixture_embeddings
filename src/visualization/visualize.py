b732db8f412b5445fe97f69c44f485ad77cb5792
eitan <eitan.turok@gmail.com>
2022-12-07 17:44:07 -0500

    plot edit distance approximation

---
+++ src/visualization/visualize.py
@@ -0,0 +1,18 @@
+import os
+import matplotlib.pyplot as plt
+
+def plot_edit_distance_approximation(y_pred, y_true, model_name, dataset, mape):
+    
+    fig, ax = plt.subplots()
+    
+    plt.hist2d(y_pred, y_true, bins=150, cmap='Blues')
+    plt.plot([0, 1], [0, 1], transform=ax.transAxes, color='red', linewidth=2)
+    
+    plt.title('Edit distance approximation\n{} MAPE={:.4f} ({})'.format(model_name, mape, dataset))
+    ax.set(xlabel='Predicted distance', ylabel='Real distance')
+   
+    filename = 'figures/{}_{}.png'.format(model_name, dataset)
+    directory = os.path.dirname(filename)
+    if directory != '' and not os.path.exists(directory):
+        os.makedirs(directory)
+    fig.savefig(filename)
