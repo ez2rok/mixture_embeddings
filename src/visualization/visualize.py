e223ac660df179a6d4268cb24b45ece1fb2d7140
eitan <eitan.turok@gmail.com>
2022-12-14 12:02:06 -0500

    display %RMSE on plot

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,14 +1,14 @@
 import os
 import matplotlib.pyplot as plt
 
-def plot_edit_distance_approximation(y_pred, y_true, model_name, dataset, mape):
+def plot_edit_distance_approximation(y_pred, y_true, model_name, dataset, percent_rmse):
     
     fig, ax = plt.subplots()
     
     plt.hist2d(y_pred, y_true, bins=150, cmap='Blues')
     plt.plot([0, 1], [0, 1], transform=ax.transAxes, color='red', linewidth=2)
     
-    plt.title('Edit distance approximation\n{} MAPE={:.4f} ({})'.format(model_name, mape, dataset))
+    plt.title('Edit distance approximation\n{} %RMSE={:.4f} ({})'.format(model_name, percent_rmse, dataset))
     ax.set(xlabel='Predicted distance', ylabel='Real distance')
    
     filename = 'reports/figures/{}_{}.png'.format(model_name, dataset)
